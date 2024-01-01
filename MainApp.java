5f736397f261787fd96ce3346dbf42951978d97b
NehaMasutage <masutageneha2104@gmail.com>
2024-01-01 14:12:31 +0530

    added MainApp

---
+++ MainApp.java
@@ -0,0 +1,10 @@
+package com.edubridge;
+
+public class MainApp {
+      
+	public static void main(String[] args) {
+		int l=9, area;
+		area=l*l;
+		System.out.println("Area of square: "+area);
+	}
+}
